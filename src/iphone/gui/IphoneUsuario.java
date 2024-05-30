2768c8f740b64361ab183d9ab82e3a5840e4430b
matheusmatta21 <matheusalexandre2004@gmail.com>
2024-05-30 09:59:45 -0300

    versao 1.0

---
+++ src/iphone/gui/IphoneUsuario.java
@@ -0,0 +1,52 @@
+package iphone.gui;
+
+import iphone.apps.AparelhoTelefonico;
+import iphone.apps.NavegadorInternet;
+import iphone.apps.ReprodutorMusical;
+
+public class IphoneUsuario implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando musica...");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Musica " + musica + " selecionada!");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para o número " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligação...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
